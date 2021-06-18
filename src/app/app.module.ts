27b3ac69d7a36d321d0935cb5bc4230a31b285e3
Ratandeep <ratandeeparun@gmail.com>
2021-06-18 11:11:49 +0530

    initail commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/home/home.component';
+import { LoginComponent } from './Home/login/login.component';
+import { RegisterComponent } from './Home/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

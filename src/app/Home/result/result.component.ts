9b87669ac0b9d3cf9ce8eb90ea5ce98bcd060d24
Ratandeep <ratandeeparun@gmail.com>
2021-06-28 21:40:19 +0530

    get result

--- src/app/Home/result/result.component.ts
+++ src/app/Home/result/result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-result',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result.component.scss']
 })
 export class ResultComponent implements OnInit {
+  result : any;
+  constructor(private api: ApiService) { }
 
-  constructor() { }
+  async ngOnInit() {
+    let user:any = localStorage.getItem('LoggedInUser')
+    
+    console.log(user)
+    let payload = {
+      "email":user
+    }
 
-  ngOnInit(): void {
+    this.api.post('/result',payload).subscribe(
+      next => {
+        console.log(next)
+      }
+    )
   }
 
 }

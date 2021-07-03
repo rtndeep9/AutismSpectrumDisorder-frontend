e32d1f54cf960406107e4a0f5b1c0e74b5d43f5d
Ratandeep <ratandeeparun@gmail.com>
2021-07-03 11:47:24 +0530

    added loading

--- src/app/Doctor/doctor-patients/doctor-patients.component.ts
+++ src/app/Doctor/doctor-patients/doctor-patients.component.ts
@@ -7,31 +7,41 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./doctor-patients.component.scss']
 })
 export class DoctorPatientsComponent implements OnInit {
-  patients:any;
-  user:any;
-  constructor(private api:ApiService) { }
+  patients: any;
+  user: any;
+  success = false;
+  url: any;
+  loading = false;
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
     this.user = localStorage.getItem('LoggedInDoctor')
     let payload = {
       "email": this.user
     }
-    this.api.post("/mypatients",payload).subscribe(next => {
+    this.api.post("/mypatients", payload).subscribe(next => {
       this.patients = next
     })
   }
 
-  connect(id:any) {
+  connect(id: any) {
     let payload = {
       "email": this.user,
-      "pid":id
+      "pid": id
     }
     console.log(payload)
-    this.api.post("/connect",payload).subscribe(next => {
+    this.loading = true;
+    this.api.post("/connect", payload).subscribe(next => {
+      this.loading = false
       console.log(next)
+      this.url = next
+      this.success = true;
     })
+    
   }
 
-
+  goToUrl(): void {
+    window.open(this.url, '_blank');
+  }
 
 }
